e31738ed9ace331f25f0742f8aded1eab0431706
david <dale.david@mail.ru>
2022-01-17 12:53:12 +0300

    dismiss ConnectionError when collecting new documents

--- skill/scraping.py
+++ skill/scraping.py
@@ -1,10 +1,20 @@
+import time
+
 import requests
 
 
 def collect_docs():
     all_items = []
     for i in range(1, 100):
-        r = requests.get('https://dialogs.yandex.ru/store/api/dialogs2', params={'page': i, 'limit': 100})
+        n = 6
+        for attempt in range(n):
+            try:
+                r = requests.get('https://dialogs.yandex.ru/store/api/dialogs2', params={'page': i, 'limit': 100})
+                break
+            except ConnectionError as e:
+                if attempt >= n - 1:
+                    raise e
+                time.sleep(2 ** attempt)
         assert r.status_code == 200
         j = r.json()
         all_items.extend(j['result']['items'])
